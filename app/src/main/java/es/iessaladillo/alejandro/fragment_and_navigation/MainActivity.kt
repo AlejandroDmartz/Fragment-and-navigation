5bb7943807574314b325f05b2293ede44a092cb8
Alejandro <alesitonightray@gmail.com>
2021-05-23 13:57:14 +0200

    Initial commit

---
+++ app/src/main/java/es/iessaladillo/alejandro/fragment_and_navigation/MainActivity.kt
@@ -0,0 +1,14 @@
+package es.iessaladillo.alejandro.fragment_and_navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+//    private lateinit var b: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
